4348669038196e0b18be64ff5399b8ea2be1a191
codewithimmy <manuimran773@gmail.com>
2022-09-09 11:28:17 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #!/bin/bash
-#include <stdio.h>
-
 /**
- * Description: main - Prints out a string to stdout.
- * Return: 0 if success.
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
  */
 
 int main(void)
-
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
